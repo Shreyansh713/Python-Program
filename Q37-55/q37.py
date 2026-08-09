f767c4f3ca868ef45f4b11995fa77ee1b6d1694a
Shreyansh <shreyanshs713@gmail.com>
2026-08-10 00:05:47 +0530

    If Else

---
+++ Q37-55/q37.py
@@ -0,0 +1,11 @@
+a,b,c = map(int,input("Enter Number : ").split())
+
+if a>b and a>c :
+    print(a,"A is Greater")
+    
+elif a<b and b>c :
+    print(b,"B is Greater")
+    
+else:
+    print(c,"C is Greater")
+
