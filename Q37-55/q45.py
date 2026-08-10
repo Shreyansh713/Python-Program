8d3c286e3842841428a325ba9d5c9485de998f5f
Shreyansh <shreyanshs713@gmail.com>
2026-08-10 17:29:37 +0530

    divisible by 3 & 7

---
+++ Q37-55/q45.py
@@ -0,0 +1,8 @@
+ch = str(input("Enter Alphabet :"))
+
+if 'A'<=ch<='Z':
+    print("Its a Uppercase Alphabet")
+elif 'a'<=ch<='z':
+    print("Its a small aphabet")
+else:
+    print("Not a alphabet")
---
+++ Q37-55/q46.py
@@ -0,0 +1,6 @@
+num = int(input("Enter number"))
+
+if num%3==0 and num%7==0:
+    print("It is divisible by 3 and 7",num)
+else:
+    print("It is not divisible by 3 and 7",num)
