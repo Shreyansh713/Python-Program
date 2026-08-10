69aaf18af54295e6f58a53849b366bf0dd0ac0c6
Shreyansh <shreyanshs713@gmail.com>
2026-08-10 21:11:36 +0530

    Electricy consume

---
+++ Q37-55/q47.py
@@ -0,0 +1,15 @@
+units = int(input("Enter Units Consumed : "))
+
+if units <= 100:
+    bill = units * 5
+
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+
+elif units <= 300:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+
+else:
+    bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 15)
+
+print("Electricity Bill :", bill)
