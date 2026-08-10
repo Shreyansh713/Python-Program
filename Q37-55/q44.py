aff60bcfce9a01868bd7ad6f0d7750b517b033b9
Shreyansh <shreyanshs713@gmail.com>
2026-08-10 17:19:40 +0530

    Check Vowel

---
+++ Q37-55/q44.py
@@ -0,0 +1,8 @@
+a = str(input("Enter Alphabet :"))
+
+if a=='a' or a=='e' or a=='i' or a=='o' or a=='u':
+    print("It is a Vowel")
+elif a=='A' or a=='E' or a=='I' or a=='O' or a=='U':
+    print("It is a vowel") 
+else:
+    print("Not a vowel")
