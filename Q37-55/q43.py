f4841457b41f59b6843ce51e5b483526a46dfc1b
Shreyansh <shreyanshs713@gmail.com>
2026-08-10 15:42:33 +0530

    Check  Alphabet up/low

---
+++ Q37-55/q43.py
@@ -0,0 +1,8 @@
+n = str(input("Enter Alphabet :"))
+
+if 'A'<=n<='Z':
+    print("Alpabet is Capital/Uppercase")
+elif 'a'<=n<='z':
+    print("Alphabet is Small/Lowercase")
+else:
+    print("Its is not a Alphabet")
